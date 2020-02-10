2a45947581a20d081e348e8576f713b91d8e7a74
zhufan <125920969@qq.com>
2020-02-10 17:40:31 +0800

    gulp脚手架的开发部分完成

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 const {
-  src, dest, parallel, series,
+  src, dest, parallel, series, watch,
 } = require('gulp');
 const babel = require('gulp-babel');
+const autoprefixer = require('gulp-autoprefixer');
+const connect = require('gulp-connect');
 
 /** 源码目录 */
 const scrPath = 'src/';
@@ -10,15 +12,56 @@ const devPath = 'dev/';
 /** 构建发布目录 */
 const buildPath = 'dist/';
 
+/**
+ * 开发部分的
+ */
+
 /** 复制lib到开发目录 */
-const TaskDevLib = () => src(`${scrPath}lib/*.*`, { allowEmpty: true }).pipe(dest(`${devPath}lib/`, { allowEmpty: true }));
+const TaskDevLib = () => src(`${scrPath}lib/*.*`, { allowEmpty: true })
+  .pipe(dest(`${devPath}lib/`, { allowEmpty: true }))
+  .pipe(connect.reload());
 /** 转换js后复制到开发目录 */
 const TaskDevJS = () => src(`${scrPath}js/*.js`, { allowEmpty: true })
   .pipe(babel())
-  .pipe(dest(`${devPath}js/`, { allowEmpty: true }));
+  .pipe(dest(`${devPath}js/`, { allowEmpty: true }))
+  .pipe(connect.reload());
+/** css自动增加前缀后复制到开发目录 */
+const TaskDevCSS = () => src(`${scrPath}css/*.css`, { allowEmpty: true })
+  .pipe(autoprefixer({ cascade: true }))
+  .pipe(dest(`${devPath}css/`))
+  .pipe(connect.reload());
+/** html自动复制到开发目录 */
+const TaskDevHTML = () => src(`${scrPath}*.html`, { allowEmpty: true })
+  .pipe(dest(devPath))
+  .pipe(connect.reload());
+/** img自动复制到开发目录 */
+const TaskDevImg = () => src(`${scrPath}img/**`, { allowEmpty: true })
+  .pipe(dest(`${devPath}img/`))
+  .pipe(connect.reload());
+/** 开发时的静态服务器 */
+const TaskDevServe = () => {
+  connect.server({
+    root: devPath,
+    livereload: true,
+  });
+};
+/** 监听所有改变 */
+const TaskDevWatch = () => {
+  watch(`${scrPath}js/*.js`, TaskDevJS);
+  watch(`${scrPath}css/*.css`, TaskDevCSS);
+  watch(`${scrPath}*.html`, TaskDevHTML);
+  watch(`${scrPath}img/**`, TaskDevImg);
+};
+
+/**
+ * 构建部分
+ */
 
 /** 开发任务 */
-const devTask = parallel(TaskDevLib, TaskDevJS);
+const devTask = series(
+  parallel(TaskDevLib, TaskDevJS, TaskDevCSS, TaskDevImg, TaskDevHTML),
+  parallel(TaskDevWatch, TaskDevServe),
+);
 /** 构建任务 */
 const buildTask = () => null;
 
--- src/js/common.js
+++ src/js/common.js
@@ -1,2 +1,109 @@
-const abc = new Promise((r) => { r([[123]].flat()); });
-abc.then((r) => console.log(r));
+/* eslint-disable no-underscore-dangle */
+/**
+ * 本方法用于计算页面根 rem 的大小
+ */
+(function init() {
+  const flexBox = () => {
+    let fontSize = document.documentElement.clientWidth / 7.5;
+    if (fontSize > 100) {
+      fontSize = 100;
+    }
+    document.documentElement.style.fontSize = `${fontSize}px`;
+    /** 在部分浏览器中，设置fontSize只会，实际1rem不等于fontSize，所以这里多一个判断，获取实际大小值 */
+    const div = document.createElement('div');
+    div.style = 'height:1rem;width:1rem';
+    document.body.appendChild(div);
+    if (Math.abs(fontSize - div.offsetHeight) > 2) {
+      fontSize = (fontSize * fontSize) / div.offsetHeight;
+      if (fontSize > 100) {
+        fontSize = 100;
+      }
+      document.documentElement.style.fontSize = `${fontSize}px`;
+    }
+    document.body.removeChild(div);
+  };
+  flexBox();
+  window.addEventListener('resize', flexBox);
+}());
+
+if (!window.tm) {
+  window.tm = {};
+}
+
+window.tm.getQuery = () => {
+  const query = window.location.search.slice(1);
+  if (!query) return {};
+  const o = {};
+  return (
+    query.split('&').forEach((e) => {
+      const t = e.split('=');
+      const n = t[0];
+      const i = t[1];
+      o[n] = i;
+    }),
+    o
+  );
+};
+
+window.tm.getAjaxUrl = (e) => {
+  const t = /^http/g.test(e) ? '' : 'http://act.baoshixingqiu.com';
+  const n = window.tm.getQuery();
+  return t + e + (e.indexOf('?') > 0 ? '&' : '?') + $.param(n);
+};
+
+window.tm.isIOS = () => window.tm.getQuery().from === 'ios';
+
+window.tm.jump2app = (e) => {
+  try {
+    if (window.Android && window.Android._jumpway) {
+      window.Android._jumpway(e);
+      return;
+    }
+
+    window[window.jsBridge].callHandler('jump', '{"jumpType":"'.concat(e, '"}'));
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.log(err);
+  }
+};
+
+window.tm.bitSeperator = (e) => {
+  const t = Number(e);
+  if (Number.isNaN(t)) return '0';
+  const n = String(e).split('.');
+  n[0] = n[0].replace(/\B(?=(\d{3})+$)/g, ',');
+  return n.join('.');
+};
+
+(() => {
+  const e = window.tm.getQuery().from === 'ios';
+  const t = e ? 'WebViewJavascriptBridge' : 'TooMeeWebViewJavascriptBridge';
+  window.jsBridge = t;
+  function n() {
+    window[t].init((env, cb) => {
+      if (cb) {
+        cb('初始化');
+      }
+    });
+  }
+  if (window[t]) {
+    // eslint-disable-next-line no-unused-expressions
+    e || n();
+  } else if (e) {
+    const i = document.createElement('iframe');
+    i.style.display = 'none';
+    i.src = 'https://__bridge_loaded__';
+    document.documentElement.appendChild(i);
+    setTimeout(() => {
+      document.documentElement.removeChild(i);
+    }, 0);
+  } else {
+    document.addEventListener(
+      'TooMeeWebViewJavascriptBridgeReady',
+      () => {
+        n();
+      },
+      !1,
+    );
+  }
+})();
